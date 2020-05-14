107a022e8553051c49b19ae82c85e7b1928586dc
Sebastian <46867564+seba0456@users.noreply.github.com>
2020-05-14 13:35:05 +0200

    V.2.3

--- main.cpp
+++ main.cpp
@@ -1,45 +1,49 @@
 #include <iostream>
 #include <conio.h>
-
+#include <windows.h>
+#include <locale.h>
 using namespace std;
 
 int main()
 {
+    setlocale(LC_CTYPE, "Polish");
+    SetConsoleTitleA( "Kalkulator stezenia procentowego by seba0456" );
     double ms;
     double mr;
     double cp;
     string instrukcja;
     string petla;
-    cout << "Hello world! V.1.0 ()seba0456" << endl;
-    cout << "Witaj w kalkulatorze stezenia procentowego!" << endl;
+    cout << "Hello world! V.2.3 ()seba0456" << endl;
+    cout << "Witaj w kalkulatorze stê¿enia procentowego!" << endl;
     cout << "Oto krotka instrukcja." << endl;
-    cout << "1.Prosze by wartosc 'Ms' byla podawana w gramach" << endl;
-    cout << "2.Prosze by wartosc 'Mr' byla podawana w gramach" << endl;
-    cout << "3.Prosze by wartosc 'Cp' byla podawana w procentach" << endl;
-    cout << "Dzieki tym zabiegom nie muszisz podawac jednostek" << endl;
+    cout << "1.Proszê by wartoœæ 'Ms' by³a podawana w gramach." << endl;
+    cout << "2.Proszê by wartoœæ 'Mr' by³a podawana w gramach." << endl;
+    cout << "3.Proszê by wartoœæ 'Cp' by³a podawana w procentach." << endl;
+    cout << "Dziêki tym zabiegom nie muszisz podawaæ jednostek." << endl;
+    cout << "W wypadku migania programu proszê go zrestartowaæ." << endl;
     cout << "Zaczynamy!" << endl;
-    cout << "Czy chcesz uruchomic program w trybie pentli? (zalecane w wypadku wielu obliczen) (y/n) eksperymentalne " << endl;
+    cout << "Czy chcesz uruchomiæ program w trybie pentli? (zalecane w wypadku wielu obliczeñ) (tak/nie) Uwaga ta funkcja jest eksperymentalna! " << endl;
     cin >> petla;
-    if ( petla == "y" )
+    if ( petla == "tak" )
     {
         while ( true )
         {
-            cout << "Podaj co chcesz obliczyc 'cp', 'ms' 'mr'" << endl;
+            cout << "Podaj co chcesz obliczyæ 'cp', 'ms' 'mr'" << endl;
     cin >> instrukcja;
 
     if ( instrukcja == "cp" )
     {
-        cout << "Podaj wartosc Ms" << endl;
+        cout << "Podaj wartoœæ Ms" << endl;
     cin >> ms;
-    cout << "Podaj watosc Mr" << endl;
+    cout << "Podaj watoœæ Mr" << endl;
     cin >> mr;
     cp = ((ms / mr) * 100);
     cout << "Dane:" << endl;
     cout << "Ms= " << ms << endl;
     cout << "Mr= " << mr << endl;
-    cout << "Wzor to (ms:mr)*100" << endl;
+    cout << "Wzór to (ms:mr)*100" << endl;
     cout << "Cp=" << cp << "%" << endl;
-    cout << "Dziekuje za uzycie narzedzia" << endl;
+    cout << "Dziêkujê za u¿ycie narzêdzia" << endl;
     cout << "seba0456" << endl;
     cin.sync();
     cin.get();
@@ -47,17 +51,17 @@ int main()
     }
     else if ( instrukcja== "ms" )
    {
-        cout << "Podaj wartosc Cp" << endl;
+        cout << "Podaj wartoœæ Cp" << endl;
     cin >> cp;
-    cout << "Podaj watosc Mr" << endl;
+    cout << "Podaj watoœæ Mr" << endl;
     cin >> mr;
     ms = ((cp * mr) / 100);
     cout << "Dane:" << endl;
     cout << "Cp= " << cp << endl;
     cout << "Mr= " << mr << endl;
-    cout << "Wzor to (Cp*mr):100%" << endl;
+    cout << "Wzór to (Cp*mr):100%" << endl;
     cout << "Ms=" << ms << "gram" << endl;
-    cout << "Dziekuje za uzycie narzedzia" << endl;
+    cout << "Dziêkujê za u¿ycie narzêdzia" << endl;
     cout << "seba0456" << endl;
     cin.sync();
     cin.get();
@@ -65,17 +69,17 @@ int main()
    }
    else if ( instrukcja== "mr" )
    {
-        cout << "Podaj wartosc Cp" << endl;
+        cout << "Podaj wartoœæ Cp" << endl;
     cin >> cp;
-    cout << "Podaj watosc Ms" << endl;
+    cout << "Podaj watoœæ Ms" << endl;
     cin >> ms;
     mr = ((ms * 100) / cp);
     cout << "Dane:" << endl;
     cout << "Cp= " << cp << endl;
     cout << "Ms= " << ms << endl;
-    cout << "Wzor to (ms*100%):cp%" << endl;
+    cout << "Wzór to (ms*100%):cp%" << endl;
     cout << "Mr=" << mr << "gram" << endl;
-    cout << "Dziekuje za uzycie narzedzia" << endl;
+    cout << "Dziêkujê za u¿ycie narzedzia" << endl;
     cout << "seba0456" << endl;
     cin.sync();
     cin.get();
@@ -108,17 +112,17 @@ int main()
     }
     else if ( instrukcja== "ms" )
    {
-        cout << "Podaj wartosc Cp" << endl;
+        cout << "Podaj wartoœæ Cp" << endl;
     cin >> cp;
-    cout << "Podaj watosc Mr" << endl;
+    cout << "Podaj watoœæ Mr" << endl;
     cin >> mr;
     ms = ((cp * mr) / 100);
     cout << "Dane:" << endl;
     cout << "Cp= " << cp << endl;
     cout << "Mr= " << mr << endl;
-    cout << "Wzor to (Cp*mr):100%" << endl;
+    cout << "Wzór to (Cp*mr):100%" << endl;
     cout << "Ms=" << ms << "gram" << endl;
-    cout << "Dziekuje za uzycie narzedzia" << endl;
+    cout << "Dziêkujê za u¿ycie narzêdzia" << endl;
     cout << "seba0456" << endl;
     cin.sync();
     cin.get();
@@ -126,17 +130,17 @@ int main()
    }
    else if ( instrukcja== "mr" )
    {
-        cout << "Podaj wartosc Cp" << endl;
+        cout << "Podaj wartoœæ Cp" << endl;
     cin >> cp;
-    cout << "Podaj watosc Ms" << endl;
+    cout << "Podaj watoœæ Ms" << endl;
     cin >> ms;
     mr = ((ms * 100) / cp);
     cout << "Dane:" << endl;
     cout << "Cp= " << cp << endl;
     cout << "Ms= " << ms << endl;
-    cout << "Wzor to (ms*100%):cp%" << endl;
+    cout << "Wzór to (ms*100%):cp%" << endl;
     cout << "Mr=" << mr << "gram" << endl;
-    cout << "Dziekuje za uzycie narzedzia" << endl;
+    cout << "Dziêkujê za u¿ycie narzedzia" << endl;
     cout << "seba0456" << endl;
     cin.sync();
     cin.get();
